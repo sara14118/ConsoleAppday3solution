06f71313f2c4d95d27e679274c9ce2797c871253
sara <sara.alzadjali14@outlook.com>
2023-11-15 15:01:37 +0400

    Add project files.

---
+++ ConsoleAppday3/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+namespace ConsoleAppday3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string s1 = "hello there";
+            //string s2 = s1;
+            //s1 = "sara";
+            //Console.WriteLine("s1 value :" +s1);
+            ////
+            //Console.WriteLine("is place :" + s1.GetHashCode());
+
+            //Console.WriteLine("s2 vlaue :" + s2);
+            //Console.WriteLine("s2 place " + s2.GetHashCode());
+
+
+
+
+
+            //int A = 10;
+
+            //string result = (A % 3 == 0) ?( $"{A} is even number" ): ($"{A} is odd number");// add dollar to allow to add value inside string 
+            //Console.WriteLine(result);
+
+
+            //int B = 10;
+
+            //string result2 = (B % 3 == 0) ? ($"{B} is Dvisable by 3") : ($"{B} is not Dvisable by 3");// add dollar to allow to add value inside string 
+            //Console.WriteLine(result2);
+
+
+
+
+
+            int x = 1;
+            Console.WriteLine(x++);
+            Console.WriteLine(x);
+
+
+            x++;
+            Console.WriteLine(x);
+             
+
+
+
+
+
+
+        }
+    }
+}
